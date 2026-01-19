ce012fcfed3ed667cd3e3f2c785179ae4eb5abeb
Tanatswa Marere <tanmarere@gmail.com>
2026-01-19 14:01:14 -0800

    added changes and foixed bugs

--- spark_jobs/injuries_etl.py
+++ spark_jobs/injuries_etl.py
@@ -16,8 +16,26 @@ spark = (
 
 
 df = spark.read.option("header", True).option("inferSchema", True).csv("s3a://pl-injuries-data-tanatswa/raw/player_injuries_impact_2024_25.csv")
+
+
 print("=== Input schema ===") 
 df.printSchema() 
 print("=== Input sample ===") 
 df.show(5, truncate=False)
 
+# Data Cleaning and Transformation
+
+df_cleaned = (
+    df
+    .withColumn("injury_start", to_date(col("Injury Start"), "yyyy-MM-dd"))
+    .withColumn("expected_return", to_date(col("Expected Return"), "yyyy-MM-dd"))
+    .withColumn("days_out", datediff(col("expected_return"), col("injury_start")))
+)
+
+             
+             #Overwrite
+df_cleaned.write.mode("overwrite").partitionBy("injury_year", "injury_month").parquet("s3a://pl-injuries-data-tanatswa/processed/player_injuries_impact_2024_25.parquet")
+
+print(type(df_cleaned))
+df_cleaned.printSchema()
+df_cleaned.show(5, truncate=False)
